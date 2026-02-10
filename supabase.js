af5c0a3dce1aa9f7d258da74b90a4c1663ab0be5
Volkez-afk <viniciusmegetto@gmail.com>
2026-02-10 15:21:45 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,10 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
 <script>
 
-const SUPABASE_URL="COLOQUE_URL"
-const SUPABASE_KEY="COLOQUE_KEY"
+const SUPABASE_URL="https://imvkhjsbchafajanurou.supabase.co"
+const SUPABASE_KEY="sb_publishable_fzy87Ttd8W_eS8DD1W6Nxg_vvkgQ-NE"
 
 window.db=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
 
 </script>
+
