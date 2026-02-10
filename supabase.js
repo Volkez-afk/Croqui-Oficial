1a60588b4775c09677a768bfe6a341add441171c
Volkez-afk <viniciusmegetto@gmail.com>
2026-02-10 15:13:32 -0300

    Update and rename supabase-config.js to supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+<script>
+
+const SUPABASE_URL="COLOQUE_URL"
+const SUPABASE_KEY="COLOQUE_KEY"
+
+window.db=supabase.createClient(SUPABASE_URL,SUPABASE_KEY)
+
+</script>
